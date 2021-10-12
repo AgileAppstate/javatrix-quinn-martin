c0af3789bbd740af889fafd74f3bc654ba9675bb
MartinHernandezGamez <32468779+MartinHernandezGamez@users.noreply.github.com>
2021-10-12 00:43:25 -0400

    Fixed formatting issues

--- javatrix/MatrixTest.java
+++ javatrix/MatrixTest.java
@@ -326,7 +326,7 @@ public class MatrixTest {
 		});
 
 		assertEquals("Cannot Subtract Unequal Size Arrays", exception.getMessage());
-    }
+    	}
 
     	@Test
 	public void testNorm11()
@@ -373,31 +373,31 @@ public class MatrixTest {
         @Test
         public void testCopy1()
         {
-            double[][] vals = {{1.0, 2.0, 3.0},{4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
+            	double[][] vals = {{1.0, 2.0, 3.0},{4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
 
-            Matrix A = new Matrix(vals);
-            Matrix B = A.copy();
+            	Matrix A = new Matrix(vals);
+            	Matrix B = A.copy();
 
-            assertEquals(A.getRows(), B.getRows());
-            assertEquals(A.getCols(), B.getCols());
-            assertArrayEquals(A.getMatrix(), B.getMatrix());
+            	assertEquals(A.getRows(), B.getRows());
+            	assertEquals(A.getCols(), B.getCols());
+            	assertArrayEquals(A.getMatrix(), B.getMatrix());
         }
 
         @Test
         public void testCopy2()
         {
-            double[][] vals = {{1.0, 2.0, 3.0},{4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
+            	double[][] vals = {{1.0, 2.0, 3.0},{4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
 
-            Matrix A = new Matrix(vals);
-            Matrix B = A.copy();
+            	Matrix A = new Matrix(vals);
+            	Matrix B = A.copy();
 
-            A.setRows(10);
-            A.setCols(10);
-            A.setMatrixPos(0, 0, 77.7);
+            	A.setRows(10);
+            	A.setCols(10);
+            	A.setMatrixPos(0, 0, 77.7);
 
-            assertFalse("Matrix rows should not be equal", A.getRows() == B.getRows());
-            assertFalse("Matrix cols should not be equal", A.getCols() == B.getCols());
-            assertFalse(Arrays.equals(A.getMatrix(), B.getMatrix()));
+            	assertFalse("Matrix rows should not be equal", A.getRows() == B.getRows());
+            	assertFalse("Matrix cols should not be equal", A.getCols() == B.getCols());
+            	assertFalse(Arrays.equals(A.getMatrix(), B.getMatrix()));
         }
 }
 
